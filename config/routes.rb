f263beac488f1e08ef546717a5ebc862a99ed308
Levi Miller <paullevimiller@gmail.com>
2019-11-11 19:58:13 -0800

    Merge game-words

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   resources :games, only: [:create, :update] do
     resources :game_words, only: [:create]
   end
-  get '/random_words', to: "words#random_words"
+  get '/words/random', to: "words#random_words"
   get '/games/high_scores', to: "games#high_scores"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
