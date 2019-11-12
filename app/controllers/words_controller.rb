e2706b8a1984eae85ca58c1772e672f0ce7b3b67
Levi Miller <paullevimiller@gmail.com>
2019-11-11 21:08:58 -0800

    Tweak default pagination for random words

--- app/controllers/words_controller.rb
+++ app/controllers/words_controller.rb
@@ -1,6 +1,6 @@
 class WordsController < ApplicationController
   def random_words
-    count = (params[:count] || "5").to_i
+    count = (params[:count] || "20").to_i
     points = params[:points]
     if points
       words = Word.all.where("points > #{points}").sample(count)
