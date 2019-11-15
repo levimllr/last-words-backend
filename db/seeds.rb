ebcfff8ab0a4fe864297cf5c3479c1ad942b4e87
Levi Miller <paullevimiller@gmail.com>
2019-11-14 16:31:56 -0800

    Comment out benchmark and byebug gems from seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,9 +5,9 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
-require 'benchmark'
+# require 'benchmark'
 require 'csv'
-require 'byebug'
+# require 'byebug'
 
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
